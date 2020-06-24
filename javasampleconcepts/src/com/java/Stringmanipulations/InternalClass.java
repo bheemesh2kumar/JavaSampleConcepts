5658ad221b5154879f9ac3a127ebc06455513d84
bheemesh <manu.bheemesh@gmail.com>
2020-06-24 19:34:38 +0530

    updated

---
+++ javasampleconcepts/src/com/java/Stringmanipulations/InternalClass.java
@@ -0,0 +1,14 @@
+package com.java.Stringmanipulations;
+
+public class InternalClass {
+
+	public static void main(String[] args) {
+		
+		
+
+		System.out.println("Test created by intrnaluser");
+
+	
+	}
+
+}
